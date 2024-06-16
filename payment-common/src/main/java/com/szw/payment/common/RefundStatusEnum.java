8a3290616130aa54b9e0aeef37f56d3e43f13ca8
Zhiwei Shao <shaozhiwei@192.168.3.19>
2024-06-16 10:03:55 +0800

    init

--- payment-common/src/main/java/com/szw/payment/common/RefundStatusEnum.java
+++ payment-common/src/main/java/com/szw/payment/common/RefundStatusEnum.java
@@ -10,11 +10,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RefundStatusEnum {
 	INIT(0, "初始化"),
+
 	WAITING_RETRY(1, "稍后重试"),
+
 	REFUND_ING(2, "退款中"),
+
 	REFUND_SUCCESS(3, "退款成功"),
+
 	REFUND_ERR(4, "退款失败"),
+
 	UNKNOWN(999, "未知");
+
 
 	private final int code;
 
