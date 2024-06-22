fe05197d1ac2525cace5b6ae0ad8f22f9697bb3c
Zhiwei Shao <shaozhiwei@192.168.3.19>
2024-06-22 18:28:17 +0800

    支持 rocketmq producer

---
+++ payment-server/src/main/java/com/szw/payment/producer/RocketMQProducer.java
@@ -0,0 +1,30 @@
+package com.szw.payment.producer;
+
+import com.szw.payment.common.utils.GsonUtil;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+@Named("rocketmq_producer")
+public class RocketMQProducer<T> implements MessageProducer<T> {
+
+	@Inject
+	private RocketMQTemplate rocketMQTemplate;
+
+
+	@Override
+	public void send(String topic, String tag, T t) {
+		StringBuilder builder = new StringBuilder(topic);
+		if (tag != null && !tag.trim().isEmpty()) {
+			builder.append(":").append(tag);
+		}
+
+		String json = GsonUtil.GSON.toJson(t);
+		Message<String> message = MessageBuilder.withPayload(json).build();
+		rocketMQTemplate.syncSend(builder.toString(), message);
+	}
+
+}
